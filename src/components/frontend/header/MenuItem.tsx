6bfc782474f14f23c544f31736b373abaff85665
Rony Hossain <mdrony.ain@gmail.com>
2024-09-19 18:34:51 +0600

    second commit

--- src/components/frontend/header/MenuItem.tsx
+++ src/components/frontend/header/MenuItem.tsx
@@ -1,9 +1,14 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import { usePathname } from 'next/navigation'
+import React, { useState } from 'react'
 
 const MenuItem = ({item}: {item: {title: string, path: string}}) => {
+  const pathname = usePathname();
+  console.log(pathname)
+  // const [selected, setSelected] = useState("");
   return (
-    <Link href={item.path}>
+    <Link href={item.path} className={`${pathname === item.path && "text-secondary"}`}>
         {item.title}
     </Link>
   )
